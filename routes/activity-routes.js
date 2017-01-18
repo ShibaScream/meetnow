d7c5ed740c150c2b8a2af14438e27ec100289dd3
Geoffrey Cox <gcox3d@gmail.com>
2017-01-17 20:55:26 -0800

    added auth to routes

--- routes/activity-routes.js
+++ routes/activity-routes.js
@@ -2,9 +2,11 @@ const authMiddleware = require('../lib/authMiddleware')
 const Activity = require('../model/activity-model')
 
 module.exports = function(router) {
-  router.post('/activity', function(req, res, next) {
-    new Activity(req.body).save().then(function(err, activity) {
-      if(err) throw(err)
+  router.post('/activity', authMiddleware, function(req, res, next) {
+    let body = req.body;
+    body.host = req.authorizedUserId
+    new Activity(body).save().then(function(err, activity) {
+      if(err) throw err
       res.json(activity)
     }).catch(next)
   })
@@ -14,16 +16,26 @@ module.exports = function(router) {
       res.json(activity)
     }).catch(next)
   })
-  router.put('/activity/:id', function(req, res, next) {
-    Activity.findByIdAndUpdate(req.params.id, {new: true}, req.body).then(function(err, activity) {
+  router.put('/activity/:id', authMiddleware, function(req, res, next) {
+    Activity.findById(req.params.id).then(function(err, activity) {
       if(err) throw err
-      res.json(activity)
+      if(activity.host == req.authorizedUserId) {
+        activity.update(req.body).save().then(function(err, activity) {
+          if(err) throw err
+          res.json(activity)
+        }).catch(next)
+      }
     }).catch(next)
   })
-  router.delete('/activity/:id', function(req, res, next) {
-    Activity.findByIdAndRemove(req.params.id).then(function(err) {
+  router.delete('/activity/:id', authMiddleware, function(req, res, next) {
+    Activity.findById(req.params.id).then(function(err, activity) {
       if(err) throw err
-      res.status(202)
+      if(activity.host == req.authorizedUserId) {
+        activity.remove(function(err) {
+          if(err) throw err
+          res.status(202)
+        })
+      }
     }).catch(next)
   })
 }
