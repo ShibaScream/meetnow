8d61d00b61a47511fdd812b1268e16f45b27eac3
Trenton Kress <trenton.kress@gmail.com>
2017-01-17 20:01:24 -0800

    Add login route to user routes.

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,12 +1,28 @@
+const parseAuth = require('basic-auth');
+
 const authMiddleware = require('../lib/authMiddleware');
 const User = require('../model/user-model');
 
 module.exports = function(router) {
-  router.post('/user/', function(req, res, next) {
+  router.post('/user', function(req, res, next) {
     new User(req.body).save().then(function(err, user) {
       if (err) throw err;
       delete user.password;
       res.json(user);
+    }).catch(next);
+  });
+
+  router.post('/login', function(req, res, next) {
+    let auth = parseAuth(req);
+    if (!auth) throw new Error('Expected authorization header.');
+    User.findOne({ email: auth.name }, function (err, user) {
+      if (err) throw err;
+      user.checkPass(auth.pass).then((correct) => {
+        if (correct)
+          res.json({ token: 'token here' });
+        else
+          throw new Error('Invalid credentials.');
+      });
     }).catch(next);
   });
 
@@ -18,6 +34,14 @@ module.exports = function(router) {
       delete user.email;
       delete user.radius;
       //possibly make gender and other personal details optional to return
+      res.json(user);
+    }).catch(next);
+  });
+
+  router.put('/user/:userId', function(req, res, next) { //TODO add pre 'update' function to schema to hash password if password was updated
+    User.findByIdAndUpdate(req.params.userId, { new: true }, req.body).then(function(err, user) {
+      if (err) throw err;
+      delete user.password;
       res.json(user);
     }).catch(next);
   });
