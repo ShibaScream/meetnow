0cff08a70d7181acf2396632bb7a8302b4733922
Geoffrey Cox <gcox3d@gmail.com>
2017-01-22 09:51:14 -0800

    test for activity get 200 and 404

---
+++ test/activity-routes-test.js
@@ -0,0 +1,47 @@
+const chai = require('chai')
+const expect = require('chai').expect
+const chaiHttp = require('chai-http')
+const app = require('../index.js')
+const Activity = require('../model/activity-model.js')
+let server = null
+let activity = null
+chai.use(chaiHttp)
+
+describe('activity-routes.js', function() {
+  before(function(done) {
+    server = app.listen(3000, function() {
+      console.log('server up')
+    })
+    activity = new Activity({
+      location: 'test',
+      startTime: Date.now(),
+      endTime: Date.now()
+    })
+    done()
+  })
+  after(function(done) {
+    server.close(function() {
+      console.log('server closed')
+    })
+    done()
+  })
+  describe('activity/:id GET', function() {
+    it('should return an activity from the database', function(done) {
+      chai.request('http://localhost:3000')
+      .get('/activitiy/01')
+      .end(function(err, res) {
+        expect(res.status).to.equal(404)
+        done()
+      })
+    })
+    it('should return an activity from the database', function(done) {
+      chai.request('http://localhost:3000')
+      .get(`/activity/${activity._id}`)
+      .end(function(err, res) {
+        console.log(activity)
+        expect(res.status).to.equal(200)
+        done()
+      })
+    })
+  })
+})
