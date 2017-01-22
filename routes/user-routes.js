2833c5c8500703ecc3bff28acb32fb4cd153b65c
Dan Peters <daniel.r.peters@icloud.com>
2017-01-22 15:56:31 -0800

    fixed bugs in code and updated user queries

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const parseAuth = require('basic-auth');
-
+const createError = require('http-errors')
 const jsonWebToken = require('jsonwebtoken');
 
 jsonWebToken.KEY = process.env.JSON_TOKEN_KEY || 'abcdef';
@@ -13,7 +13,7 @@ const User = require('../model/user-model');
 module.exports = function(router) {
   router.post('/user', function(req, res, next) {
     new User(req.body).save().then(function(err, user) {
-      if (err) throw err;
+      if (err) next(err);
       delete user.password;
       res.json(user);
     }).catch(next);
@@ -21,50 +21,69 @@ module.exports = function(router) {
 
   router.get('/login', function(req, res, next) {
     let auth = parseAuth(req);
-    if (!auth) throw new Error('Expected authorization header.');
-    User.findOne({ email: auth.name }, function (err, user) {
-      if (err) throw err;
-      user.checkPass(auth.pass, (err, correct) => {
-        if (err) throw err;
-        if (correct) {
-          let token = jsonWebToken.sign(user, jsonWebToken.KEY, {
-            expiresIn: TOKEN_EXPIRY_TIME
-          });
+    if (!auth) next(createError(403, 'Expected authorization header.'))
+    User
+      .findOne({ email: auth.name }, function (err, user) {
+        if (err) return next(err)
+        user
+          .checkPass(auth.pass, (err, correct) => {
+            if (err) return next(err)
+            if (correct) {
+              let token = jsonWebToken.sign(user, jsonWebToken.KEY, {
+                expiresIn: TOKEN_EXPIRY_TIME
+              });
 
-          res.json({
-            token: token,
-            expiresIn: Date.now() + (TOKEN_EXPIRY_TIME * 1000)
-          });
-        } else
-          throw new Error('Invalid credentials.');
-      });
-    }).catch(next);
-  });
+              res.json({
+                token: token,
+                expiresIn: Date.now() + (TOKEN_EXPIRY_TIME * 1000)
+              });
+            } else
+              next(createError(403, 'Invalid credentials.'))
+          })
+      })
+      .catch(next)
+  })
 
   router.get('/user/:userId', authMiddleware, function(req, res, next) {
-    User.findOneById(req.params.userId || req.authorizedUserId).then(function(err, user) {
-      if (err) throw err;
-      delete user.password;
-      delete user.email;
-      delete user.radius;
-      //possibly make gender and other personal details optional to return
-      res.json(user);
-    }).catch(next);
+    User
+      .findById(req.params.userId || req.authorizedUserId)
+      .populate({
+        path: 'interests',
+        model: 'interest',
+        populate: {
+          path: 'category',
+          model: 'category'
+        }
+      })
+      .then(user => {
+        user.password = undefined
+        user.email = undefined
+        user.radius = undefined
+        //possibly make gender and other personal details optional to return
+        res.json(user)
+      })
+      .catch(next)
   });
 
   router.put('/user', authMiddleware, function(req, res, next) { //TODO add pre 'update' function to schema to hash password if password was updated
-    User.findByIdAndUpdate(req.authorizedUserId, { new: true }, req.body).then(function(err, user) {
-      if (err) throw err;
-      delete user.password;
-      res.json(user);
-    }).catch(next);
-  });
+    User
+      .findByIdAndUpdate(req.authorizedUserId, { new: true }, req.body)
+      .then(function(err, user) {
+        if (err) return next(err)
+        delete user.password;
+        res.json(user);
+      })
+      .catch(next);
+  })
 
   router.delete('/user/:userId', authMiddleware, function(req, res, next) {
-    User.findByIdAndRemove(req.params.userId).then(function(err) {
-      if (err) throw err;
-      res.status(202);
-      res.end();
-    }).catch(next);
+    User
+      .findByIdAndRemove(req.params.userId)
+      .then(function(err) {
+        if (err) return next(err)
+        res.status(202);
+        res.end();
+      })
+      .catch(next);
   });
 };
