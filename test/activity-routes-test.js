9d7e8d6c405248c0a61278db3c41c3278200e493
Geoffrey Cox <gcox3d@gmail.com>
2017-01-22 10:43:27 -0800

    setting up before block to get token for testUser -- currently not working

--- test/activity-routes-test.js
+++ test/activity-routes-test.js
@@ -1,10 +1,13 @@
+'use strict'
 const chai = require('chai')
 const expect = require('chai').expect
 const chaiHttp = require('chai-http')
 const app = require('../index.js')
-const Activity = require('../model/activity-model.js')
+// const Activity = require('../model/activity-model.js')
+const User = require('../model/user-model.js')
 let server = null
 let activity = null
+let testUser = null // <--- maybe
 chai.use(chaiHttp)
 
 //query for user and findOne name: whatever
@@ -14,6 +17,21 @@ describe('activity-routes.js', function() {
     server = app.listen(3000, function() {
       console.log('server up')
     })
+    User.findOne({name: 'Runs More'})
+    .then(function(err, user) {
+      //something err
+      testUser = user
+    })
+    //login in the testUser to get token
+    chai.request(app)
+    .post('/login')
+    .auth('runs.more@test.com', '1234pass')
+    .end(function(err, res) {
+      
+      //assign token to something
+      console.log(res)
+    })
+
     // remove this junk
     // activity = new Activity({
     //   location: 'test',
@@ -30,7 +48,7 @@ describe('activity-routes.js', function() {
   })
   describe('activity/:id GET', function() {
     it('should return an activity from the database', function(done) {
-      chai.request('http://localhost:3000')
+      chai.request(app)
       .get('/activitiy/01')
       .end(function(err, res) {
         expect(res.status).to.equal(404)
@@ -38,7 +56,7 @@ describe('activity-routes.js', function() {
       })
     })
     it('should return an activity from the database', function(done) {
-      chai.request('http://localhost:3000')
+      chai.request(app)
       .get(`/activity/${activity._id}`)
       .end(function(err, res) {
         console.log(activity)
