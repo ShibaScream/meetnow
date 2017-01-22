6c704be7ee61c75a7b78dae287bf56ecac6a7db7
Trenton Kress <trenton.kress@gmail.com>
2017-01-22 11:04:00 -0800

    Add radius based activity search.

--- routes/activity-routes.js
+++ routes/activity-routes.js
@@ -3,6 +3,8 @@
 const authMiddleware = require('../lib/authMiddleware')
 const Activity = require('../model/activity-model')
 
+const MILES_PER_DEG = 55.2428;
+
 module.exports = function(router) {
   router.post('/activity', authMiddleware, function(req, res, next) {
     let body = req.body;
@@ -12,12 +14,14 @@ module.exports = function(router) {
       res.json(activity)
     }).catch(next)
   })
+
   router.get('/activity/:id', function(req, res, next) {
     Activity.findById(req.params.id).then(function(err, activity) {
       if (err) throw err
       res.json(activity)
     }).catch(next)
   })
+
   router.put('/activity/:id', authMiddleware, function(req, res, next) {
     Activity.findById(req.params.id).then(function(err, activity) {
       if(err) throw err
@@ -29,6 +33,7 @@ module.exports = function(router) {
       }
     }).catch(next)
   })
+
   router.delete('/activity/:id', authMiddleware, function(req, res, next) {
     Activity.findById(req.params.id).then(function(err, activity) {
       if(err) throw err
@@ -40,4 +45,23 @@ module.exports = function(router) {
       }
     }).catch(next)
   })
+
+  router.get('/activity/search', function(req, res, next) {
+    let radius = req.query.radius;
+    let lat = req.query.lat;
+    let lng = req.query.lng;
+    let activitiesWithin = [];
+    if (radius && lat != undefined && lng != undefined) {
+      Activity.find({}).then(function(activities) {
+        activitiesWithin = activities.filter(function(activity) {
+          return distanceInMiles(activity.startLocation.coordinates[0], activity.startLocation.coordinates[1], lat, lng) < radius;
+        });
+      }).catch(next);
+    }
+    res.json(activitiesWithin);
+  });
+}
+
+function distanceInMiles(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) * MILES_PER_DEG;
 }
