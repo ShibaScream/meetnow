c13847c3051839c85fbc7f403918c4d40b201343
Dan Peters <daniel.r.peters@icloud.com>
2017-01-22 16:18:08 -0800

    added authentication to all activity routes because they are connected to users, updated formatting

--- routes/activity-routes.js
+++ routes/activity-routes.js
@@ -9,57 +9,76 @@ module.exports = function(router) {
   router.post('/activity', authMiddleware, function(req, res, next) {
     let body = req.body;
     body.host = req.authorizedUserId
-    new Activity(body).save().then(function(err, activity) {
-      if(err) throw err
-      res.json(activity)
-    }).catch(next)
+    new Activity(body)
+      .save()
+      .then(function(err, activity) {
+        if(err) throw err
+        res.json(activity)
+      })
+      .catch(next)
   })
 
   router.get('/activity/:id', function(req, res, next) {
-    Activity.findById(req.params.id).then(function(err, activity) {
-      if (err) throw err
-      res.json(activity)
-    }).catch(next)
+    Activity
+      .findById(req.params.id)
+      .then(function(err, activity) {
+        if (err) throw err
+        res.json(activity)
+      })
+      .catch(next)
   })
 
   router.put('/activity/:id', authMiddleware, function(req, res, next) {
-    Activity.findById(req.params.id).then(function(err, activity) {
-      if(err) throw err
-      if(activity.host == req.authorizedUserId) {
-        activity.update(req.body).save().then(function(err, activity) {
-          if(err) throw err
-          res.json(activity)
-        }).catch(next)
-      }
-    }).catch(next)
+    Activity
+      .findById(req.params.id)
+      .then(function(err, activity) {
+        if(err) throw err
+        if(activity.host == req.authorizedUserId) {
+          activity
+            .update(req.body)
+            .save()
+            .then(function(err, activity) {
+              if(err) throw err
+              res.json(activity)
+            })
+            .catch(next)
+        }
+      })
+      .catch(next)
   })
 
   router.delete('/activity/:id', authMiddleware, function(req, res, next) {
-    Activity.findById(req.params.id).then(function(err, activity) {
-      if(err) throw err
-      if(activity.host == req.authorizedUserId) {
-        activity.remove(function(err) {
-          if(err) throw err
-          res.status(202)
-        })
-      }
-    }).catch(next)
+    Activity
+      .findById(req.params.id)
+      .then(function(err, activity) {
+        if(err) throw err
+        if(activity.host == req.authorizedUserId) {
+          activity.remove(function(err) {
+            if(err) throw err
+            res.status(202)
+          })
+        }
+      })
+      .catch(next)
   })
 
-  router.get('/activity/search', function(req, res, next) {
-    let radius = req.query.radius;
-    let lat = req.query.lat;
-    let lng = req.query.lng;
-    let activitiesWithin = [];
+  router.get('/activity/search', authMiddleware, function(req, res, next) {
+    let radius = req.query.radius
+    let lat = req.query.lat
+    let lng = req.query.lng
+    let activitiesWithin = []
     if (radius && lat != undefined && lng != undefined) {
-      Activity.find({}).then(function(activities) {
-        activitiesWithin = activities.filter(function(activity) {
-          return distanceInMiles(activity.startLocation.coordinates[0], activity.startLocation.coordinates[1], lat, lng) < radius;
-        });
-        res.json(activitiesWithin);
-      }).catch(next);
+      Activity
+        .find({})
+        .then(function(activities) {
+          activitiesWithin = activities.filter(function(activity) {
+            return distanceInMiles(activity.startLocation.coordinates[0], activity.startLocation.coordinates[1], lat, lng) < radius
+          })
+          res.json(activitiesWithin)
+        })
+        .catch(next)
     } else {
-      next(new Error('Expected radius, lat, and lng'));
+      next(new Error('Expected radius, lat, and lng'))
     }
   });
 }
