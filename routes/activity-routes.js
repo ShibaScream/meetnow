822a28505ef8b380d98a19000562dd9c7e51bbc6
Dan Peters <daniel.r.peters@icloud.com>
2017-01-22 17:46:27 -0800

    fixed geo query for activities

--- routes/activity-routes.js
+++ routes/activity-routes.js
@@ -6,13 +6,46 @@ const Activity = require('../model/activity-model')
 const MILES_PER_DEG = 55.2428;
 
 module.exports = function(router) {
+
+  router.get('/activity/search', function(req, res, next) {
+    let dist = req.query.dist || 10000
+    let lat = req.query.lat
+    let lng = req.query.lng
+    // let activitiesWithin = []
+    if (dist && lat != undefined && lng != undefined) {
+      let coords = [lng, lat]
+      Activity
+      .find({
+        startLocation: {
+          $nearSphere: {
+            $geometry: {
+              type: 'Point',
+              coordinates: coords
+            },
+            $minDistance: 0,
+            $maxDistance: dist
+          }
+        }
+      })
+      .then(activities => {
+        // activitiesWithin = activities.filter(function(activity) {
+        //   return distanceInMiles(activity.startLocation.coordinates[0], activity.startLocation.coordinates[1], lat, lng) < radius
+        // })
+        res.json(activities)
+      })
+      .catch(next)
+    } else {
+      next(new Error('Expected radius, lat, and lng'))
+    }
+  })
+
   router.post('/activity', authMiddleware, function(req, res, next) {
     let body = req.body;
     body.host = req.authorizedUserId
+    console.log(body)
     new Activity(body)
       .save()
-      .then(function(err, activity) {
-        if(err) throw err
+      .then(activity => {
         res.json(activity)
       })
       .catch(next)
@@ -21,8 +54,7 @@ module.exports = function(router) {
   router.get('/activity/:id', authMiddleware, function(req, res, next) {
     Activity
       .findById(req.params.id)
-      .then(function(err, activity) {
-        if (err) throw err
+      .then(activity => {
         res.json(activity)
       })
       .catch(next)
@@ -31,14 +63,12 @@ module.exports = function(router) {
   router.put('/activity/:id', authMiddleware, function(req, res, next) {
     Activity
       .findById(req.params.id)
-      .then(function(err, activity) {
-        if(err) throw err
+      .then(activity => {
         if(activity.host == req.authorizedUserId) {
           activity
             .update(req.body)
             .save()
-            .then(function(err, activity) {
-              if(err) throw err
+            .then(activity => {
               res.json(activity)
             })
             .catch(next)
@@ -50,8 +80,7 @@ module.exports = function(router) {
   router.delete('/activity/:id', authMiddleware, function(req, res, next) {
     Activity
       .findById(req.params.id)
-      .then(function(err, activity) {
-        if(err) throw err
+      .then(activity => {
         if(activity.host == req.authorizedUserId) {
           activity.remove(function(err) {
             if(err) throw err
@@ -62,25 +91,6 @@ module.exports = function(router) {
       .catch(next)
   })
 
-  router.get('/activity/search', authMiddleware, function(req, res, next) {
-    let radius = req.query.radius
-    let lat = req.query.lat
-    let lng = req.query.lng
-    let activitiesWithin = []
-    if (radius && lat != undefined && lng != undefined) {
-      Activity
-        .find({})
-        .then(function(activities) {
-          activitiesWithin = activities.filter(function(activity) {
-            return distanceInMiles(activity.startLocation.coordinates[0], activity.startLocation.coordinates[1], lat, lng) < radius
-          })
-          res.json(activitiesWithin)
-        })
-        .catch(next)
-    } else {
-      next(new Error('Expected radius, lat, and lng'))
-    }
-  });
 }
 
 function distanceInMiles(x1, y1, x2, y2) {
